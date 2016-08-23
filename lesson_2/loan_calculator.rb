4b4721802a24dec27f02230d0db6bee304f5bf28
Phillip Johnson <pdjohnson84@gmail.com>
2016-08-22 23:07:18 -0700

    Starting work on loan calculator

---
+++ lesson_2/loan_calculator.rb
@@ -0,0 +1,15 @@
+# Need to retrieve:
+#Loan amount
+#APR
+#Loan duration
+#calculate using m = p * (j / (1 - (1 + j)**-n))
+# where:
+# m = monthly payment
+# p = loan amount
+# j = monthly interest rate
+# n = loan duration in months
+
+
+def prompt(message)
+  puts("=> #{message}")
+end
