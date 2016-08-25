f3cb43b89a76c0f4b6dc5555ab651216afac9a8b
Phillip Johnson <pdjohnson84@gmail.com>
2016-08-24 21:34:44 -0700

    Added lizard and spock

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -1,13 +1,15 @@
-VALID_CHOICES = %w(rock paper scissors)
+VALID_CHOICES = %w(rock paper scissors lizard spock)
 
 def prompt(message)
   puts("=> #{message}")
 end
 
 def win?(first, second)
-  (first == 'rock' && second == 'scissors') ||
-    (first == 'paper' && second == 'rock') ||
-    (first == 'scissors' && second == 'paper')
+  (first == 'rock' && (second == 'scissors' || second == 'lizard')) ||
+    (first == 'paper' && (second == 'rock' || second == 'spock')) ||
+    (first == 'scissors' && (second == 'paper' || second == 'lizard')) ||
+    (first == 'lizard' && (second == 'paper' || second == 'spock')) ||
+    (first == 'spock' && (second == 'scissors' || second == 'rock'))
 end
 
 def display_results(player, computer)
@@ -21,24 +23,51 @@ def display_results(player, computer)
 end
 
 loop do
-  choice = ''
+  player_score = 0
+  computer_score = 0
   loop do
-    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-    choice = gets.chomp
+    choice = ''
+    loop do
+      prompt("Choose one: #{VALID_CHOICES.join(', ')}")
 
-    if VALID_CHOICES.include?(choice)
-      break
-    else
-      prompt("That's not a valid choice.")
+      choice = case gets.chomp
+               when 'r'
+                 choice = 'rock'
+               when 'p'
+                 choice = 'paper'
+               when 'sc'
+                 choice = 'scissors'
+               when 'sp'
+                 choice = 'spock'
+               when 'l'
+                 choice = 'lizard'
+               end
+
+      if VALID_CHOICES.include?(choice)
+        break
+      else
+        prompt("That's not a valid choice.")
+      end
     end
+
+    computer_choice = VALID_CHOICES.sample
+
+    prompt("You chose: #{choice}; Computer chose: #{computer_choice}")
+
+    display_results(choice, computer_choice)
+
+    if win?(choice, computer_choice)
+      player_score += 1
+    else
+      computer_score += 1
+    end
+
+    prompt("Your score is #{player_score}.
+      The computer score is #{computer_score}")
+
+    break unless player_score < 5
+    break unless computer_score < 5
   end
-
-  computer_choice = VALID_CHOICES.sample
-
-  prompt("You chose: #{choice}; Computer chose: #{computer_choice}")
-
-  display_results(choice, computer_choice)
-
   prompt("Do you want to play again?")
   answer = gets.chomp
   break unless answer.downcase.start_with?('y')
